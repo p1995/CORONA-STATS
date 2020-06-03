da535da45e7cc8a220dfe99be844c48de123e542
p1995 <pj1311995@gmail.com>
2020-06-03 15:47:10 +0530

    corona-header

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,22 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 function Header() {
   return (
     <div>
       <header>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/CoronaCases">CoronaCases</Link>
-            </li>
-            <li>
-              <Link to="/StateWiseCases">StateWiseCases</Link>
-            </li>
-            {/* <li>
-              <Link to="/schedule">Schedule</Link>
-            </li> */}
-          </ul>
-        </nav>
+        <Navbar bg="dark" variant="dark" fixed="top">
+          ><Navbar.Brand>COVID-19</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto justify-content-end">
+              <Nav.Link>
+                <Link to="/CoronaCases">Daily-Stats</Link>
+              </Nav.Link>
+              <Nav.Link>
+                <Link to="/StateWiseCases">State Cases</Link>
+              </Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
       </header>
     </div>
   );
